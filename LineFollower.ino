88f5bec38fa33c29cb41d93dae8721cd0505ff4a
richlay <lsps950204@gmail.com>
2021-05-14 09:55:02 +0800

    added lineCount

--- LineFollower.ino
+++ LineFollower.ino
@@ -1,6 +1,9 @@
-void fx_lineFollower() {
-  
-  while(1) {
+int lineCount = 0;
+bool on_line = false;
+
+void fx_lineFollower(int linesToCross) {
+
+  while (lineCount <= linesToCross) {
     fx_CarDirection(readLED()); //assuming max turning-degree = 35deg (one side)
   }
 }
@@ -11,11 +14,11 @@ double readLED() {
   static unsigned long presentTime = 0;
   presentTime = millis();
   static double deltaTime = 0;
-  deltaTime = double(presentTime - previousTime)/1000.0;
-  if(previousTime==0) {
+  deltaTime = double(presentTime - previousTime) / 1000.0;
+  if (previousTime == 0) {
     deltaTime = 0;
   }
-  
+
   static int LED[7] = {0};
   LED[2] = digitalRead(2);
   LED[3] = digitalRead(3);
@@ -27,13 +30,26 @@ double readLED() {
   static double LED_counts = 0;
   LED_counts = LED[2] + LED[3] + LED[4] + LED[5] + LED[6];
 
+  //lines count
+  if (on_line == false) {
+    if (LED_counts >= 3) {
+      lineCount++;
+      on_line = true;
+    }
+  }
+  else if (on_line == true) {
+    if (LED_counts < 3) {
+      on_line = false;
+    }
+  }
+
   //ex: If LED[5] and LED[6] both read black line(output = 1, 2, respectively), then LEDxWeight(w/o PID) is 1.5
   static int weight[7] = {0, 0, -2, -1, 0, 1, 2};
   static double LEDxWeight = 0;
   LEDxWeight = LED[2] * weight[2] + LED[3] * weight[3] + LED[4] * weight[4] + LED[5] * weight[5] + LED[6] * weight[6];
   LEDxWeight = PID(LEDxWeight, deltaTime);
   previousTime = presentTime;
-  return (LEDxWeight/LED_counts);
+  return (LEDxWeight / LED_counts);
 }
 
 double PID(double error, double dT) {
@@ -42,7 +58,7 @@ double PID(double error, double dT) {
   static double P_term = 0;
   P_term = P * error;
   static double errorSum = 0;
-  errorSum = errorSum + (error*dT);
+  errorSum = errorSum + (error * dT);
   static double I_term = 0;
   I_term = I * errorSum;
   return (P_term + I_term);
